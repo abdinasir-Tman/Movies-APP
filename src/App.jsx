c1c253f2cdfdec0e15d67c14e60faf56d593fae3
Engr-Pirlo <omarjibrilabdulkhadir@gmail.com>
2023-08-12 22:28:43 +0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import  './index.css'
+import Header from './components/Header'
+import Model from './components/Model'
+import MoveContainer from './components/MoveContainer'
+import Movie from './components/Movie'
+
+function App() {
+
+  return (
+   <div className="bg-blue-700 h-screen px-5">
+
+     <Header />
+     <MoveContainer  />
+     <Movie />
+     <Model />
+
+   </div>
+  
+    
+     
+
+
+
+  
+  )
+}
+
+export default App
